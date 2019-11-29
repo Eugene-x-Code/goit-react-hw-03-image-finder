e0493053be6471cb5c87daef56ce03649a33cfb1
Eugene-x-Code <evgeni.iv.xy@gmail.com>
2019-11-30 00:20:30 +0200

    Updates

---
+++ precache-manifest.01be94ffc303157c561bdf7f41223a13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7024b6142d395cb3fcd1c9143898e101",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "321e80e0d638fe9366b0",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.c0b82aa3.chunk.css"
+  },
+  {
+    "revision": "1a0f6804f5829cacee12",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.793c947b.chunk.js"
+  },
+  {
+    "revision": "321e80e0d638fe9366b0",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.93b42b13.chunk.js"
+  },
+  {
+    "revision": "3dc2d4bc992ea5be91b7",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.7345331c.js"
+  }
+]);
