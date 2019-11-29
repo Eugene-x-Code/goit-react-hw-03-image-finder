d4f8d8b84f1a1c95db1741e7e307cd536e06132c
Eugene-x-Code <evgeni.iv.xy@gmail.com>
2019-11-30 00:04:57 +0200

    pre commit

--- src/Components/ImageFinder/Modal/FullSizeModal.jsx
+++ src/Components/ImageFinder/Modal/FullSizeModal.jsx
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { Component, createRef } from 'react';
 import Modal from 'react-awesome-modal';
 import PropTypes from 'prop-types';
 import css from './FullSizeModal.module.scss';
 
-const FullSizeModal = ({ close, visible, fullImage }) => (
-  <Modal visible={visible} effect="fadeInDown" onClickAway={close}>
-    <div>
-      <img className={css.modalImage} src={fullImage} alt="#" />
-    </div>
-  </Modal>
-);
+class FullSizeModal extends Component {
+  state = {};
+
+  backDropRef = createRef();
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyPress);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyPress);
+  }
+
+  handleKeyPress = e => {
+    if (e.code !== 'Escape') return;
+    this.props.close();
+  };
+
+  render() {
+    return (
+      <Modal
+        visible={this.props.visible}
+        effect="fadeInDown"
+        onClickAway={this.props.close}
+      >
+        <div>
+          <img className={css.modalImage} src={this.props.fullImage} alt="#" />
+        </div>
+      </Modal>
+    );
+  }
+}
 
 FullSizeModal.propTypes = {
   close: PropTypes.func.isRequired,
